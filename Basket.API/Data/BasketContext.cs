e0fbe4e121386f6d15b43af24aacdfef40865909
Israel Barbara <israelsilvabarbara@gmail.com>
2025-03-21 13:28:20 -0300

    Fix changed FluentValidation.ASP to FluentValidation and add IValidation manual verification

--- Basket.API/Data/BasketContext.cs
+++ Basket.API/Data/BasketContext.cs
@@ -6,35 +6,18 @@ namespace Basket.API.Data
 {
     public class BasketContext : DbContext
     {
-        public DbSet<BasketSelection> Baskets { get; init; }
-
-        public BasketContext(DbContextOptions options)
+        public DbSet<BasketSelection> Baskets { get; set; }
+        
+        public BasketContext(DbContextOptions<BasketContext> options, ILogger<BasketContext> logger)
             : base(options)
-        {
-        }
+        {}
+
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<BasketSelection>().ToCollection("baskets");
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-            var dbHost = Environment.GetEnvironmentVariable("DB_HOST") ?? "localhost";
-            var dbPort = Environment.GetEnvironmentVariable("DB_PORT") ?? "27017"; // Default MongoDB port
-            var dbUser = Environment.GetEnvironmentVariable("DB_USER");
-            var dbPass = Environment.GetEnvironmentVariable("DB_PASS");
-            var dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? "BasketDB";
- 
-            // Construct the connection string
-            var connectionString = string.IsNullOrWhiteSpace(dbUser) || string.IsNullOrWhiteSpace(dbPass)
-                ? $"mongodb://{dbHost}:{dbPort}" // Without authentication
-                : $"mongodb://{dbUser}:{dbPass}@{dbHost}:{dbPort}";
- 
-            optionsBuilder.UseMongoDB(connectionString, dbName);
-
         }
     }
 
--- Basket.API/Endpoints/BasketEndpoints.cs
+++ Basket.API/Endpoints/BasketEndpoints.cs
@@ -1,4 +1,8 @@
 using Basket.API.Data;
+using Basket.API.DTOs;
+using Basket.API.Models;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -8,22 +12,33 @@ namespace Basket.API.Endpoints
     {
         public static void MapBasketEndpoints(this IEndpointRouteBuilder app)
         {
-            app.MapGet("/basket/{username}", listBasketItems)
-                .WithName("GetBasket")
-                .WithOpenApi();
-            app.MapPost("/basket", insertBasketItem);
-            app.MapPut("/basket", updateBasket);
-            app.MapPut("/basket/add/{productId}", addBasketItemAmout);
-            app.MapPut("/basket/sub/{productId}", subBasketItemAmout);
-            app.MapDelete("/basket", deleteBasket);
-            app.MapDelete("/basket/{productId}", deleteBasketItem);
+            app.MapGet("/basket",listBaskets);
+            app.MapGet("/basket/{buyerId}", listBasketItems);
+            app.MapPost("/basket/item", insertBasketItem);
+            app.MapPut("/basket/item", updateBasket);
+            app.MapPut("/basket/item/increment", incrementBasketItemAmout);
+            app.MapPut("/basket/item/decrement", decrementBasketItemAmout);
+            app.MapDelete("/basket/{buyerId}", deleteBasket);
+            app.MapDelete("/basket/item", deleteBasketItem);
         }
 
 
-
-        public static async Task<IResult> listBasketItems(string buyer, BasketContext context)
+        public static async Task<IResult> listBaskets( [FromServices] BasketContext context )
         {
-            var basket = await context.Baskets.FirstOrDefaultAsync(b => b.BuyerId == buyer);
+            var baskets = await context.Baskets.ToListAsync();
+
+
+            Console.WriteLine("DEBUG: Baskets: " + baskets);
+            //Console.WriteLine("INFO: Baskets: " + JsonSerializer.Serialize(baskets));
+            return Results.Ok(baskets);
+        }
+
+
+        public static async Task<IResult> listBasketItems(
+            [FromRoute] string buyerId, 
+            [FromServices] BasketContext context )
+        {
+            var basket = await context.Baskets.FirstOrDefaultAsync(b => b.BuyerId == buyerId);
 
             if (basket == null)
             {
@@ -36,70 +51,108 @@ namespace Basket.API.Endpoints
         }
 
 
-        public static async Task<IResult> insertBasketItem(string buyer , BasketItemRequest item, BasketContext context)
+        public static async Task<IResult> insertBasketItem( 
+            [FromBody] CreateBasketItemRequest request, 
+            [FromServices] IValidator<CreateBasketItemRequest> validator, 
+            [FromServices] BasketContext context)
         {
-            var basket =  await context.Baskets.FirstOrDefaultAsync(b => b.BuyerId == buyer);
+
+            var validationResult = validator.Validate(request);
+
+            if (!validationResult.IsValid)
+            {
+                return Results.BadRequest(validationResult.ToDictionary());
+            }
+
+            var basket =  await context.Baskets.FirstOrDefaultAsync(b => b.BuyerId == request.BuyerId);
 
             if (basket == null)
             {
-                return Results.NotFound();
+                basket  = new BasketSelection
+                {
+                    BuyerId = request.BuyerId,
+                    Items = new List<Models.BasketItem>(),
+                };
+
+                await context.Baskets.AddAsync(basket);
             }
 
-            var BasketItem = basket.Items.FirstOrDefault( i => i.ProductId == item.ProductId);
+            var BasketItem = basket.Items.FirstOrDefault( i => i.ProductId == request.ProductId);
 
             if (BasketItem == null)
             {
                 basket.Items.Add(new Models.BasketItem
                 {
-                    ProductId = item.ProductId,
-                    ProductName = item.ProductName,
-                    Quantity = item.Quantity,
-                    UnitPrice = item.UnitPrice,
-                    PictureUrl = item.PictureUrl
+                    ProductId = request.ProductId,
+                    ProductName = request.ProductName,
+                    Quantity = request.Quantity,
+                    UnitPrice = request.UnitPrice,
+                    PictureUrl = request.PictureUrl
                 });
             }
             else
             {
-                BasketItem.Quantity += item.Quantity;
+                BasketItem.Quantity += request.Quantity;
             }
 
             await context.SaveChangesAsync();
 
             return Results.Ok();
         }
-        private static async Task<IResult> updateBasket(string buyer, BasketItemRequest item,BasketContext context)
+
+        private static async Task<IResult> updateBasket(
+            [FromBody] UpdateBasketItemRequest request,
+            [FromServices] IValidator<UpdateBasketItemRequest> validator,
+            [FromServices] BasketContext context )
         {
-            var basket = await context.Baskets.FirstOrDefaultAsync(b => b.BuyerId == buyer);
+            var validationResult = validator.Validate(request);
+
+            if( !validationResult.IsValid )
+            {
+                return Results.BadRequest(validationResult.ToDictionary());
+            }
+
+            var basket = await context.Baskets.FirstOrDefaultAsync(b => b.BuyerId == request.BuyerId);
 
             if (basket == null)
             {
                 return Results.NotFound();
             }
 
-            var BasketItem = basket.Items.FirstOrDefault(i => i.ProductId == item.ProductId);
+            var BasketItem = basket.Items.FirstOrDefault(i => i.ProductId == request.ProductId);
 
             if (BasketItem == null)
             {
                 return Results.NotFound();
             }
 
-            BasketItem.Quantity = item.Quantity;
+            BasketItem.Quantity = request.Quantity;
 
             await context.SaveChangesAsync();
 
             return Results.Ok();
         }
 
-        private static async Task<IResult> addBasketItemAmout(string buyer,int productId,BasketContext context)
+        private static async Task<IResult> incrementBasketItemAmout(
+            [FromBody] BasketItemRequest request,
+            [FromServices] IValidator<BasketItemRequest> validator,
+            [FromServices] BasketContext context )
         {
-            var basket = await context.Baskets.FirstOrDefaultAsync(b => b.BuyerId == buyer);
+            var validationResult = validator.Validate(request);
+
+            if (!validationResult.IsValid)
+            {
+                return Results.BadRequest(validationResult.ToDictionary());
+            }
+
+            var basket = await context.Baskets.FirstOrDefaultAsync(b => b.BuyerId == request.BuyerId);
 
             if (basket == null)
             {
                 return Results.NotFound();
             }
 
-            var basketItem = basket.Items.FirstOrDefault(i => i.ProductId == productId);
+            var basketItem = basket.Items.FirstOrDefault(i => i.ProductId == request.ProductId);
 
             if (basketItem == null)
             {
@@ -113,16 +166,26 @@ namespace Basket.API.Endpoints
             return Results.Ok();
         }
 
-        private static async Task<IResult> subBasketItemAmout( string buyer,int productId, BasketContext context)
+        private static async Task<IResult> decrementBasketItemAmout( 
+            [FromBody] BasketItemRequest request, 
+            [FromServices] IValidator<BasketItemRequest> validator,
+            [FromServices] BasketContext context )
         {
-            var basket = await context.Baskets.FirstOrDefaultAsync(b => b.BuyerId == buyer);
+            var validationResult = validator.Validate(request);
+
+            if( !validationResult.IsValid )
+            {
+                return Results.BadRequest(validationResult.ToDictionary());
+            }
+
+            var basket = await context.Baskets.FirstOrDefaultAsync(b => b.BuyerId == request.BuyerId);
 
             if (basket == null)
             {
                 return Results.NotFound();
             }
 
-            var basketItem = basket.Items.FirstOrDefault(i => i.ProductId == productId);
+            var basketItem = basket.Items.FirstOrDefault(i => i.ProductId == request.ProductId);
 
             if (basketItem == null)
             {
@@ -133,11 +196,14 @@ namespace Basket.API.Endpoints
             {
                 basketItem.Quantity--;
             }
+            //UNCOMMENT IF YOU WANT TO DELETE THE ITEM
+            /* 
             else
             {
-                // COULD BE IGNORED DEPENDING ON BUSINESS LOGIC
+                // COULD BE IGNORED DEPENDING ON BUSINESS LOGIC 
                 basket.Items.Remove(basketItem);
-            }
+            } 
+            */
 
             await context.SaveChangesAsync();
 
@@ -146,8 +212,15 @@ namespace Basket.API.Endpoints
         }
 
 
-        private static async Task<IResult> deleteBasket( string buyer,BasketContext context)
+        private static async Task<IResult> deleteBasket( 
+            string buyer,
+            [FromServices] BasketContext context )
         {
+            if (string.IsNullOrEmpty(buyer))
+            {
+                return Results.BadRequest();
+            }
+
             var basket = await context.Baskets.FirstOrDefaultAsync(b => b.BuyerId == buyer);
 
             if (basket == null)
@@ -162,16 +235,26 @@ namespace Basket.API.Endpoints
             return Results.Ok();
         }
 
-        private static async Task<IResult> deleteBasketItem(string buyer,int productId,BasketContext context)
+        private static async Task<IResult> deleteBasketItem(
+            [FromBody] BasketItemRequest request,
+            [FromServices] IValidator<BasketItemRequest> validator,
+            [FromServices] BasketContext context )
         {
-            var basket = await context.Baskets.FirstOrDefaultAsync(b => b.BuyerId == buyer);
+            var validationResult = validator.Validate(request);
+
+            if (!validationResult.IsValid)
+            {
+                return Results.BadRequest(validationResult.ToDictionary());
+            }
+
+            var basket = await context.Baskets.FirstOrDefaultAsync(b => b.BuyerId == request.BuyerId);
 
             if(basket == null)
             {
                 return Results.NotFound();
             }
 
-            var BasketItem = basket.Items.FirstOrDefault(i => i.ProductId == productId);
+            var BasketItem = basket.Items.FirstOrDefault(i => i.ProductId == request.ProductId );
 
             if (BasketItem == null)
             {
@@ -183,7 +266,6 @@ namespace Basket.API.Endpoints
             await context.SaveChangesAsync();
 
             return Results.Ok();
-        }
-        
+        }        
     }
 }
